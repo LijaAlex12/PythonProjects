9072759937bb2be0decd7994c9999693fb092a31
Lija Alex <48295613+LijaAlex12@users.noreply.github.com>
2020-03-22 02:06:52 +0530

    calculator_higher_order.py

---
+++ calculator_higher_order.py
@@ -0,0 +1,20 @@
+def add(num1, num2):
+    sum = num1 + num2
+    return sum
+
+
+def multiply(num1, num2):
+    pdt = num1 * num2
+    return pdt
+
+# higher order function: a function that takes as input another function
+#the function that is input as parameter called CALLBACK function
+
+
+def calculator(num1, num2, operator):
+    return operator(num1, num2)
+
+
+print(calculator(1, 2, add))
+print(calculator(3,4,multiply))
+print('\n')
